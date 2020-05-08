13e4297316920872fb6e56c71be9dc3966834616
Olivia Cubela <oliviacubela@gmail.com>
2020-05-08 02:53:06 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 def fizz(int)
-  if int % 3 == 0 # if the number int is divisible by 3
+  if int % 3 = 0 # if the number int is divisible by 3
     "Fizz" # Go fizz
   end
 end
 
 def buzz(int)
-  if int % 5 == 0
+  if int % 5 = 0
     "Buzz"
   end
 end

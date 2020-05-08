e5dde53bb50f42cfedd01639273db3426f33b8a1
Olivia Cubela <oliviacubela@gmail.com>
2020-05-08 03:51:06 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,5 @@
-# Don't forget! This file needs to be 'required' in its spec file
-# See README.md for instructions on how to do this
+def fizzbuzz(number)
+  return 'FizzBuzz' if number % 15 == 0
+  return 'Fizz' if number % 3 == 0
+  return 'Buzz' if number % 5 == 0
+end
